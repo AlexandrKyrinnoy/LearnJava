d033eaf457a05421ab212d968e71d7bc4c1bf357
Alexandr Kyrinnoy <alexandr.kyrinnoy@gmail.com>
2017-11-03 15:10:19 +0200

    add some functionals

---
+++ src/com/learn/classRepository/SomeAdditionalFunctions.java
@@ -0,0 +1,11 @@
+package com.learn.classRepository;
+
+/**
+ * Created by Owner on 03.11.2017.
+ */
+public class SomeAdditionalFunctions {
+
+    public int square() {
+        return 10 * 10;
+    }
+}
