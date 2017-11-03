2cf746e4634a5d06d9c3dba1a2de91f21613c7d6
Alexandr Kyrinnoy <alexandr.kyrinnoy@gmail.com>
2017-11-03 11:54:23 +0200

    add BoxDemo

--- src/com/learn/examples/BoxDemo.java
+++ src/com/learn/examples/BoxDemo.java
@@ -1,12 +1,24 @@
 package com.learn.examples;
 
+import com.learn.classRepository.Box;
+
 /**
  * Created by Owner on 03.11.2017.
  */
 public class BoxDemo {
 
     public static void main(String[] args) {
-        
-        System.out.println("BoxDemp as work!");
+        Box myBox = new Box();
+        double vol;
+
+        myBox.width = 10;
+        myBox.height = 20;
+        myBox.depth = 15;
+
+        vol = myBox.width * myBox.height * myBox.depth;
+
+        System.out.println("Volume : " + vol);
+
+
     }
 }
