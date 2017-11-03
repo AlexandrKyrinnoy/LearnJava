13b3eb58939479c587c78840f430a42a500b7096
Alexandr Kyrinnoy <alexandr.kyrinnoy@gmail.com>
2017-11-03 11:37:23 +0200

    created class Box

---
+++ src/com/learn/classRepository/Box.java
@@ -0,0 +1,10 @@
+package com.learn.classRepository;
+
+/**
+ * Created by Owner on 03.11.2017.
+ */
+public class Box {
+    public double width;
+    public double height;
+    public double depth;
+}
---
+++ src/com/learn/examples/BoxDemo.java
@@ -0,0 +1,12 @@
+package com.learn.examples;
+
+/**
+ * Created by Owner on 03.11.2017.
+ */
+public class BoxDemo {
+
+    public static void main(String[] args) {
+        
+        System.out.println("BoxDemp as work!");
+    }
+}
