5dbddf6b5a988fa649683f4f87e2618727481535
Alexandr Kyrinnoy <alexandr.kyrinnoy@gmail.com>
2017-11-08 09:16:09 +0200

    add Outer class

---
+++ src/com/learn/examples/OuterDemo.java
@@ -0,0 +1,14 @@
+package com.learn.examples;
+
+import com.learn.classRepository.Outer;
+
+/**
+ * Created by Owner on 08.11.2017.
+ */
+public class OuterDemo {
+    public static void main(String[] args) {
+
+        Outer outer = new Outer();
+        outer.test();
+    }
+}
