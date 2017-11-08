5af37949deb3d2433e222b7d5d52c428d846889c
Alexandr Kyrinnoy <alexandr.kyrinnoy@gmail.com>
2017-11-08 14:43:51 +0200

    19100

--- src/com/learn/examples/VAPassArrayExample.java
+++ src/com/learn/examples/VAPassArrayExample.java
@@ -12,6 +12,13 @@ public class VAPassArrayExample {
         System.out.println();
     }
 
+    static void vaTest2(int ... v) {
+        System.out.print("Number of args: " + v.length + ", Contains: ");
+        for(int x: v)
+            System.out.print(x + " ");
+        System.out.println();
+    }
+
     static void VAPassArrayExampleDemo1() {
         int n1[] = {10};
         int n2[] = {11, 12, 13};
@@ -22,7 +29,14 @@ public class VAPassArrayExample {
         vaTest1(n3);
     }
 
+    static void VAPassArrayExampleDemo2() {
+        vaTest2(20);
+        vaTest2(21, 22, 23, 24);
+        vaTest2();
+    }
+
+
     public static void main(String[] args) {
-        VAPassArrayExampleDemo1();
+        VAPassArrayExampleDemo2();
     }
 }
