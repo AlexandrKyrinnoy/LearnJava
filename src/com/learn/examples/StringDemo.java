62ca377da96309560b5530e73266756571e685e5
Alexandr Kyrinnoy <alexandr.kyrinnoy@gmail.com>
2017-11-08 12:02:13 +0200

    18700 - String

---
+++ src/com/learn/examples/StringDemo.java
@@ -0,0 +1,16 @@
+package com.learn.examples;
+
+/**
+ * Created by Owner on 08.11.2017.
+ */
+public class StringDemo {
+    public static void main(String[] args) {
+        String strObj1 = "The first string";
+        String strObj2 = "a econd string.";
+        String strObj3 = strObj1 + " and " + strObj2;
+
+        System.out.println(strObj1);
+        System.out.println(strObj2);
+        System.out.println(strObj3);
+    }
+}
